acd8c946c4251a43072b18c874b780fc6d3e8410
Егорка <egorkaevd210@gmail.com>
2024-06-24 19:53:16 +0300

    commit

--- src/main/java/ru/nokisev/profit/models/Post.java
+++ src/main/java/ru/nokisev/profit/models/Post.java
@@ -1,4 +1,22 @@
 package ru.nokisev.profit.models;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.ManyToAny;
+
+import java.util.Date;
+
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String desc;
+    private Date dateOfCreated;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id")
+    private User author;
+
 }
--- src/main/java/ru/nokisev/profit/models/User.java
+++ src/main/java/ru/nokisev/profit/models/User.java
@@ -1,4 +1,26 @@
 package ru.nokisev.profit.models;
 
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.List;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(nullable = false)
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String avatar;
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "users")
+    private List<Post> posts;
+
 }
